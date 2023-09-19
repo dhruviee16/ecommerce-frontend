2d2c927e7da412371352f8b71bc61ae2e7b95fbd
prachigadhiya <gadhiyaprachi89@gmail.com>
2023-09-19 11:08:44 +0530

    feat: added profile page

--- src/components/Home/page.tsx
+++ src/components/Home/page.tsx
@@ -1,11 +1,12 @@
 'use client';
 
+import { Profile } from '@/modules/profile';
 import Image from 'next/image';
+import Link from 'next/link';
 import poster from 'public/image/HomeImages/vector19.png';
+import banner from 'public/image/HomeImages/vector6.jpg';
 import CategoryCircles from './CategoryCircles';
 import HomeProducts1 from './HomeProducts';
-import banner from 'public/image/HomeImages/vector6.jpg';
-import Link from 'next/link';
 
 function HomePage() {
   return (
@@ -64,13 +65,13 @@ function HomePage() {
           </div>
           <div className="mt-4">
             <Link href="#">
-            <button className="bg-white text-black sm:bg-black sm:text-white py-2 px-4 border-black border-2 text-center font-medium text-sm hover:bg-white transition-colors duration-200 hover:text-black">
-              SHOP NOW
-            </button>
-          </Link>
+              <button className="bg-white text-black sm:bg-black sm:text-white py-2 px-4 border-black border-2 text-center font-medium text-sm hover:bg-white transition-colors duration-200 hover:text-black">
+                SHOP NOW
+              </button>
+            </Link>
           </div>
         </div>
-        <div className='relative'>
+        <div className="relative">
           <Image
             src="/image/HomeImages/row.jpg"
             alt="Knives,Brushes,Pallets"
@@ -87,16 +88,16 @@ function HomePage() {
             </div>
             <div className="mt-4">
               <Link href="#">
-              <button className="bg-white text-black sm:bg-black sm:text-white py-2 px-4 border-black border-2 text-center font-medium text-sm hover:bg-white transition-colors duration-200 hover:text-black">
-              SHOP NOW
-            </button>
+                <button className="bg-white text-black sm:bg-black sm:text-white py-2 px-4 border-black border-2 text-center font-medium text-sm hover:bg-white transition-colors duration-200 hover:text-black">
+                  SHOP NOW
+                </button>
               </Link>
             </div>
           </div>
         </div>
-        </div>
+      </div>
 
-        {/* <div className="flex-col ml-12 gap-y-16 hidden md:flex">
+      {/* <div className="flex-col ml-12 gap-y-16 hidden md:flex">
           <div className="">
             <Image
               src="/image/HomeImages/row.jpg"
@@ -114,30 +115,32 @@ function HomePage() {
             />
           </div>
         </div> */}
-      
-      <div className= "relative m-10 p-10 text-center">
+
+      <div className="relative m-10 p-10 text-center">
         <Image
           src={banner}
           alt="Knives,Brushes,Pallets"
           className="object-cover object-center w-[100%] h-[50vh] "
         />
         <div className="flex flex-col justify-center items-center md:block md:absolute md:left-96 md:top-24">
-            <div className="m-5 md:m-0">
-              <div className="text-black md:text-start font-bold text-xl md:text-sm gap-2">
-                <div className="text-center md:text-start text-xl md:text-xl">
-                CURATING CREATIVITY THROUGH OUR ARTFUL SUPPLIES  
-                
-            {/* <div className="mt-4"> */}
-              <Link href="#">
-              <button className="bg-white text-black sm:bg-black sm:text-white py-2 px-4 border-black border-2 text-center font-medium text-sm hover:bg-white transition-colors duration-200 hover:text-black ml-4">
-              SHOP NOW
-            </button> 
-              </Link>
-            {/* </div> */}
+          <div className="m-5 md:m-0">
+            <div className="text-black md:text-start font-bold text-xl md:text-sm gap-2">
+              <div className="text-center md:text-start text-xl md:text-xl">
+                CURATING CREATIVITY THROUGH OUR ARTFUL SUPPLIES
+                {/* <div className="mt-4"> */}
+                <Link href="#">
+                  <button className="bg-white text-black sm:bg-black sm:text-white py-2 px-4 border-black border-2 text-center font-medium text-sm hover:bg-white transition-colors duration-200 hover:text-black ml-4">
+                    SHOP NOW
+                  </button>
+                </Link>
+                {/* </div> */}
+                <div>
+                  <Profile />
                 </div>
               </div>
             </div>
           </div>
+        </div>
       </div>
     </>
   );
